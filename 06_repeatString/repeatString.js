95cf8eff1b402223b77d68f958867a17514e7cc8
Falade Joseph <faladejoseph400@gmail.com>
2025-09-25 05:55:15 +0100

    Give two arguments to the function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
 };
 
+console.log(repeatString);
 // Do not edit below this line
 module.exports = repeatString;
