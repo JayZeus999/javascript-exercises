d565200a41640edd740303806438882d90a98d93
Falade Joseph <faladejoseph400@gmail.com>
2025-09-25 11:20:17 +0100

    Create function to reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,9 +1,9 @@
 const reverseString = function(string) {
-    let result = []
-    for (i = 0; i < string.length; i++){
-        return result += string.split('');
+    let result = " ";
+    for (i = string.length - 1; i >= 0; i--){
+        result += string[i];
     }
-
+    return result;
 };
 
 console.log(reverseString("hello there"))
