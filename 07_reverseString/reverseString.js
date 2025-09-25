c3e2e573169fc9c663c50d70ba9a64d847c64086
Falade Joseph <faladejoseph400@gmail.com>
2025-09-25 10:48:33 +0100

    Split string into an array

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = []
+    for (i = 0; i < string.length; i++){
+        return result += string.split('');
+    }
 
 };
 
+console.log(reverseString("hello there"))
 // Do not edit below this line
 module.exports = reverseString;
