c04361778669e1c3d4b57aa166cc94c9d3616644
Falade Joseph <faladejoseph400@gmail.com>
2025-09-25 11:42:09 +0100

    Remove single value from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(num) {
+    return num != 2;
 };
 
+const array = [1, 2, 3, 4]
+const removeArg = array.filter(removeFromArray);
+console.log(removeArg);
 // Do not edit below this line
 module.exports = removeFromArray;
