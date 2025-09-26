e422238e131da6ad7290826504eb78daef311048
Falade Joseph <faladejoseph400@gmail.com>
2025-09-26 11:09:55 +0100

    Create Celsius converter

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let finalTempC = (((temp - 32) * 5) / 9);
+  let roundTempC = Math.round(finalTempC * 10) / 10;
+  return (roundTempC);
 };
+
+const finalCTemp = convertToCelsius(32);
+console.log(`${finalCTemp} is the temp in Celsius`);
 
 const convertToFahrenheit = function() {
 };
+
 
 // Do not edit below this line
 module.exports = {
