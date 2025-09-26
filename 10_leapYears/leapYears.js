e7e738348c9c05e0b2ca2b0f1a757aaa9fd078d1
Falade Joseph <faladejoseph400@gmail.com>
2025-09-26 10:45:14 +0100

    Create function for leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if((year % 4 === 0) && (year % 100 !== 0)){
+        return ("leap year")
+    }else if((year % 4 === 0) && (year % 100 === 0) && (year % 400 !== 0)){
+        return ("not leap year")
+    }else if((year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0)){
+        return ("leap year")
+    }
 };
 
+const leapYr = leapYears(2004);
+console.log(`${leapYr}`);
 // Do not edit below this line
 module.exports = leapYears;
+
+
