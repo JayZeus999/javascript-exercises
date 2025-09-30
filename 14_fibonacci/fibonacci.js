069b1bd829f788b0b5d21d21ea3653c2f540e971
Falade Joseph <faladejoseph400@gmail.com>
2025-09-30 21:35:23 +0100

    Add loop to fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let a = 1;
+    let b = 1;
+
+    for (i = 2; i < num; i++){
+        let tempNum = a + b;
+
+        a = b;
+        b = tempNum;
+    }
+    return b;
 };
+
+console.log(fibonacci(4)); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+console.log(fibonacci(6)); // returns 8
 
 // Do not edit below this line
 module.exports = fibonacci;
