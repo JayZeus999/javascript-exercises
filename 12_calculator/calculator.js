8379829ea6fdb447a69f9594d35c99c7652140e7
Falade Joseph <faladejoseph400@gmail.com>
2025-09-30 20:12:31 +0100

    Add contents of needed functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,64 @@
-const add = function() {
-	
+const add = function () {
+  let num1 = 20;
+  let num2 = 20;
+  let sum = num1 + num2;
+  return sum;
+};
+console.log(add());
+
+
+const subtract = function () {
+  let num1 = 30;
+  let num2 = 20;
+  let diff = num1 - num2;
+  return diff;
+};
+console.log(subtract());
+
+
+const sum = function () {
+  const arr = [];
+
+  let sumArr = 0;
+  for (i = 0; i <= arr.length; i++) {
+    sumArr += i;
+  }
+  return sumArr;
 };
 
-const subtract = function() {
-	
+console.log(sum());
+
+
+const multiply = function () {
+  const arr = [1, 2, 3, 4];
+
+  let product = 1;
+  for (let num of arr) {
+    product *= num;
+  }
+  return product;
 };
 
-const sum = function() {
-	
+console.log(multiply());
+
+
+const power = function (base, exponent) {
+  let pwr = base ** exponent;
+  return pwr;
+};
+console.log(power(2, 3));
+
+const factorial = function () {
+  let num = 8;
+  let fact = 1;
+
+  for (i = num; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(factorial());
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +69,4 @@ module.exports = {
   power,
   factorial
 };
+
