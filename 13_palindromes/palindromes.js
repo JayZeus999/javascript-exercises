e36ed42af4cd32226d7078f8b6cb33e2bf77d2d6
Falade Joseph <faladejoseph400@gmail.com>
2025-09-30 20:54:38 +0100

    Add palindrome check

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
 const palindromes = function () {
-
+    let string = "racecar"
+    let palin = string.split("").reverse().join("");
+    
+    if (palin === string){
+        return true;
+    }
 };
+console.log(palindromes());
 
 // Do not edit below this line
 module.exports = palindromes;
