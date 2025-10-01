09791f9852bedfed86cc0f77becdb9d5e34d01bf
Falade Joseph <faladejoseph400@gmail.com>
2025-10-01 10:05:28 +0100

    Rearrange for absent year properties

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,24 +1,34 @@
-const findTheOldest = function(array) {
-    return array.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+const findTheOldest = function (array) {
+   let sorted = array.sort((a, b) => {
+    // if no yearOfDeath, use current year
+    const lastYearA = a.yearOfDeath || new Date().getFullYear();
+    const lastYearB = b.yearOfDeath || new Date().getFullYear();
+
+    const ageA = lastYearA - a.yearOfBirth;
+    const ageB = lastYearB - b.yearOfBirth;
+
+    return ageB - ageA; // oldest first
+  });
+
+  return sorted[0].name; // return name of oldest
 };
 
 const people = [
-      {
+    {
         name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-      },
-      {
+        yearOfBirth: 2018,
+    },
+    {
         name: "Ray",
         yearOfBirth: 1962,
         yearOfDeath: 2011,
-      },
-      {
+    },
+    {
         name: "Jane",
         yearOfBirth: 1912,
         yearOfDeath: 1941,
-      },
-    ]
+    },
+]
 
 console.log(findTheOldest(people));
 
